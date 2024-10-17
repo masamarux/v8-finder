54b1043c1e85cd3e33b2759e4ac35ac8e219d9e2
Marcelo Alves <alves-marcelo97@hotmail.com>
2024-10-17 00:31:59 -0300

    add:(vehicles module) add correct imports

--- src/vehicles/vehicles.module.ts
+++ src/vehicles/vehicles.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { VehiclesService } from './vehicles.service';
 import { VehiclesResolver } from './vehicles.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
-import { VehicleMake } from './schemas/vehicle-make.schema';
+import { VehicleMake, VehicleMakeSchema } from './schemas/vehicle-make.schema';
+import { VehicleType, VehicleTypeSchema } from './schemas/vehicle-type.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: VehicleMake.name, schema: VehicleMake.schema },
+      { name: VehicleMake.name, schema: VehicleMakeSchema },
+      { name: VehicleType.name, schema: VehicleTypeSchema },
     ]),
   ],
   providers: [VehiclesService, VehiclesResolver],
